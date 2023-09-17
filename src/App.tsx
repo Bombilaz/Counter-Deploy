49fbe83f02cc00482bda1fce746ef92fef62fdd1
Малыхин Александр <malihinau@answer-42.ru>
2023-09-17 18:41:48 +0300

    created vite 2

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useLocation } from "react-router-dom";
+import { PATHS } from "./main.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const { pathname } = useLocation();
 
   return (
     <>
       <div>
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        {(pathname === PATHS.MAIN || pathname === PATHS.VITE) && <img src={viteLogo} className="logo" alt="Vite logo" />}
+        {(pathname === PATHS.MAIN || pathname === PATHS.REACT) && <img src={reactLogo} className="logo react" alt="React logo" />}
       </div>
       <h1>Тестовый проект для деплоя Фронтенд-Приложения</h1>
       <div className="card">
--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,35 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+
+export enum PATHS {
+  MAIN = '/',
+  REACT = '/react',
+  VITE = '/vite'
+}
+
+const router = createBrowserRouter([
+  {
+    path: PATHS.MAIN,
+    element: <App />,
+  },
+  {
+    path: PATHS.REACT,
+    element: <App />,
+  },
+  {
+    path: PATHS.VITE,
+    element: <App />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
